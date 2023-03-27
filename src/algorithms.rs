7f4f72063774e82976025854c1aa22bc7bcf040b
NWrenger <nils@wrenger.net>
2023-03-27 23:52:43 +0200

    :rocket: Initial Commit

---
+++ src/algorithms.rs
@@ -0,0 +1,48 @@
+pub fn nils_quicksort(a: &mut [i32]) {
+    let pivot = if let Some (&pivot) =  a.last() {
+        pivot
+    } else {
+        return;
+    };
+    let mut index = 0;
+    for current in 0..a.len() - 1 {
+        if a[current] < pivot {
+            a.swap(index, current);
+            index += 1;
+        }
+    }
+    let last = a.len() - 1;
+    a.swap(index, last);
+    let (lower, upper) = a.split_at_mut(index);
+    nils_quicksort(lower);
+    nils_quicksort(&mut upper[1..]); // ignoring pivot element
+}
+
+
+pub fn anton_quicksort(a: &mut [i32]) {
+    if a.is_empty() {
+        return;
+    }
+    let pivot = a[a.len() / 2];
+
+    let mut i = 0;
+    let mut j = a.len() - 1;
+    loop {
+        while a[i] < pivot.clone() {
+            i += 1;
+        }
+
+        while a[j] > pivot.clone() {
+            j -= 1;
+        }
+
+        if i >= j {
+            break;
+        }
+
+        a.swap(i, j);
+    }
+
+    anton_quicksort(&mut a[..i]);
+    anton_quicksort(&mut a[j + 1..]);
+}
---
+++ src/main.rs
@@ -0,0 +1,69 @@
+mod algorithms;
+use clap::{Parser, ValueEnum};
+use rand::prelude::*;
+use rand::rngs::SmallRng;
+
+#[derive(Parser)]
+#[command(author, version, about)]
+struct Args {
+    #[arg(value_enum)]
+    implementation: Implementation,
+    #[arg(short, long, default_value_t = 1000)]
+    length: usize,
+    #[arg(short, long, default_value_t = 0)]
+    seed: u64,
+}
+
+#[derive(Clone, ValueEnum)]
+enum Implementation {
+    Unsorted,
+    Rust,
+    Nils,
+    Anton,
+}
+
+fn main() {
+    let Args {
+        implementation,
+        length,
+        seed,
+    } = Args::parse();
+    let mut unsorted = generate(length, seed);
+    // let mut unsorted: Vec<i32> = (0..length as i32).collect();
+    // let mut unsorted = vec![2, 1, 3, -4, -6];
+    match implementation {
+        Implementation::Unsorted => {
+            println!("Unsorted: {unsorted:?}");
+        }
+        Implementation::Rust => {
+            unsorted.sort();
+            assert!(is_sorted(&unsorted), "{unsorted:?}");
+        }
+        Implementation::Nils => {
+            algorithms::nils_quicksort(&mut unsorted);
+            assert!(is_sorted(&unsorted), "{unsorted:?}");
+        }
+        Implementation::Anton => {
+            algorithms::anton_quicksort(&mut unsorted);
+            assert!(is_sorted(&unsorted), "{unsorted:?}");
+        }
+    }
+}
+
+fn is_sorted(slice: &[i32]) -> bool {
+    for pair in slice.windows(2) {
+        if pair[0] > pair[1] {
+            return false;
+        }
+    }
+    true
+}
+
+fn generate(len: usize, seed: u64) -> Vec<i32> {
+    let mut rng = SmallRng::seed_from_u64(seed);
+    let mut vec = Vec::with_capacity(len);
+    for _ in 0..len {
+        vec.push(rng.gen());
+    }
+    vec
+}
