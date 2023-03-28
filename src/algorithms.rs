2168f3fb512e6f27cbf9e5bdf31ba7915107192b
NWrenger <nils@wrenger.net>
2023-03-28 15:48:43 +0200

    :sparkles: A little bit algorithm optimizing

--- src/algorithms.rs
+++ src/algorithms.rs
@@ -1,21 +1,32 @@
 pub fn nils_quicksort(a: &mut [i32]) {
-    let pivot = if let Some(&pivot) = a.last() {
-        pivot
-    } else {
-        return;
-    };
-    let mut index = 0;
-    for current in 0..a.len() - 1 {
-        if a[current] < pivot {
-            a.swap(index, current);
-            index += 1;
+    let mut sorted = false;
+    for pair in a.windows(2) {
+        if pair[0] > pair[1] {
+            sorted = true;
+            break;
+        } else {
+            sorted = false;
         }
     }
-    let last = a.len() - 1;
-    a.swap(index, last);
-    let (lower, upper) = a.split_at_mut(index);
-    nils_quicksort(lower);
-    nils_quicksort(&mut upper[1..]); // ignoring pivot element
+    if sorted {
+        let pivot = if let Some(&pivot) = a.last() {
+            pivot
+        } else {
+            return;
+        };
+        let mut index = 0;
+        for current in 0..a.len() - 1 {
+            if a[current] < pivot {
+                a.swap(index, current);
+                index += 1;
+            }
+        }
+        let last = a.len() - 1;
+        a.swap(index, last);
+        let (lower, upper) = a.split_at_mut(index);
+        nils_quicksort(lower);
+        nils_quicksort(&mut upper[1..]); // ignoring pivot element
+    }
 }
 
 pub fn anton_quicksort(a: &mut [i32]) {
