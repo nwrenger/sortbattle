e77d4c30e2a8c51c2d531145c18645499b7bc639
NWrenger <nils@wrenger.net>
2023-04-03 12:57:05 +0200

    :lipstick: FIxed Naming

--- src/algorithms.rs
+++ src/algorithms.rs
@@ -1,6 +1,6 @@
-pub fn nils_quicksort(a: &mut [i32]) {
+pub fn nils_quicksort(vec: &mut [i32]) {
     let mut sorted = false;
-    for pair in a.windows(2) {
+    for pair in vec.windows(2) {
         if pair[0] > pair[1] {
             sorted = true;
             break;
@@ -9,21 +9,21 @@ pub fn nils_quicksort(a: &mut [i32]) {
         }
     }
     if sorted {
-        let pivot = if let Some(&pivot) = a.last() {
+        let pivot = if let Some(&pivot) = vec.last() {
             pivot
         } else {
             return;
         };
         let mut index = 0;
-        for current in 0..a.len() - 1 {
-            if a[current] < pivot {
-                a.swap(index, current);
+        for current in 0..vec.len() - 1 {
+            if vec[current] < pivot {
+                vec.swap(index, current);
                 index += 1;
             }
         }
-        let last = a.len() - 1;
-        a.swap(index, last);
-        let (lower, upper) = a.split_at_mut(index);
+        let last = vec.len() - 1;
+        vec.swap(index, last);
+        let (lower, upper) = vec.split_at_mut(index);
         nils_quicksort(lower);
         nils_quicksort(&mut upper[1..]); // ignoring pivot element
     }
